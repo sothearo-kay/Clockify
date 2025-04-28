cf8a59911ca316601e10dd461ab5f4cb6a0644e8
sothearo-kay <sothearo.kay@techostartup.center>
2025-04-28 15:12:59 +0700

    feat(dev): add moti

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,24 @@
-import { SafeAreaView, StyleSheet } from "react-native";
+import { SafeAreaView, Text, StyleSheet } from "react-native";
+import { MotiView } from "moti";
 
 export default function HomeScreen() {
-  return <SafeAreaView style={styles.container}></SafeAreaView>;
+  return (
+    <SafeAreaView style={styles.container}>
+      <MotiView
+        from={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ type: "timing" }}
+      >
+        <Text>Hello, world!</Text>
+      </MotiView>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: "center",
     backgroundColor: "#fafafa",
   },
 });
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,7 @@ import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
 import "react-native-reanimated";
+import "react-native-gesture-handler";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();

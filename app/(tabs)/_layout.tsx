808bd5269e5d447068dfe1ca6c5266ecabfab459
sothearo-kay <sothearo.kay@techostartup.center>
2025-04-28 15:46:42 +0700

    feat(tabs): add icon to tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,18 @@
-import { Tabs } from "expo-router";
 import { Platform } from "react-native";
+import { Tabs } from "expo-router";
+import { Colors } from "@/constants/colors";
+import { IconSymbol } from "@/components/ui/iconSymbol";
+import { HapticTab } from "@/components/hapticTab";
+import TabBarBackground from "@/components/ui/tabBarBackground";
 
 export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
+        tabBarActiveTintColor: Colors.tint,
         headerShown: false,
+        tabBarButton: HapticTab,
+        tabBarBackground: TabBarBackground,
         tabBarStyle: Platform.select({
           ios: {
             // Use a transparent background on iOS to show the blur effect
@@ -15,10 +22,42 @@ export default function TabLayout() {
         }),
       }}
     >
-      <Tabs.Screen name="index" options={{ title: "Home" }} />
-      <Tabs.Screen name="history" options={{ title: "History" }} />
-      <Tabs.Screen name="profile" options={{ title: "Profile" }} />
-      <Tabs.Screen name="settings" options={{ title: "Settings" }} />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={24} name="house.fill" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="history"
+        options={{
+          title: "History",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={24} name="clock.arrow.circlepath" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={24} name="person.crop.circle" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: "Settings",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={24} name="gearshape.fill" color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
---
+++ constants/colors.ts
@@ -0,0 +1,15 @@
+/**
+ * below are the colors that are used in the app. the colors are defined in the light and dark mode.
+ * there are many other ways to style your app. for example, [nativewind](https://www.nativewind.dev/), [tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
+ */
+
+const tintcolorlight = "#3b82f6";
+
+export const Colors = {
+  text: "#181818",
+  background: "#fafafa",
+  tint: tintcolorlight,
+  icon: "#687076",
+  tabicondefault: "#687076",
+  tabiconselected: tintcolorlight,
+};
